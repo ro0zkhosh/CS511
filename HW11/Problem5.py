40a55570310897a4f989f2d6972c3853d571756f
shahin <shahinroozkhosh@gmail.com>
2020-12-11 23:42:10 -0500

    commit message

--- HW11/Problem5.py
+++ HW11/Problem5.py
@@ -1,8 +1,8 @@
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5a.py
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5b.py
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5c.py
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5d.py
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5e.py
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5f.py
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5g.py
-https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5h.py
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5a.in
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5b.in
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5c.in
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5d.in
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5e.in
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5f.in
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5g.in
+https://github.com/ro0zkhosh/CS511/blob/master/HW11/Problem5h.in
